1874e4ab9f2b90b0542b23e530fa1288ac43c76f
Indranil <indranil1102@gmail.com>
2023-12-07 22:48:51 -0400

    refactor: methods

--- src/action_cli.py
+++ src/action_cli.py
@@ -16,42 +16,53 @@ def api_request(url, token, method="GET", data=None, params=None):
 
 def download_artifact(artifacts, designite_output_old, token):
     '''Download an artifact from a given artifact name'''
-    
-    for artifact in artifacts:
-        if artifact["name"] == designite_output_old:
-            resp = api_request(artifact["archive_download_url"], token)
-            if resp.status_code != 200:
-                print(f"Failed to download artifact '{artifact['name']}'.")
-                return False
 
-            # Save the downloaded artifact to a local file - This saves in the current repository checkout directory
-            with open(f'{artifact["name"]}.zip', 'wb') as f:
-                f.write(resp.content)
+    filtered_artifacts = list(filter(lambda artifact: artifact["name"] == designite_output_old, artifacts))
 
-            os.makedirs(artifact["name"], exist_ok=True)
-            # Extract the downloaded artifact
-            with zipfile.ZipFile(f'{artifact["name"]}.zip', 'r') as zip_ref:
-                zip_ref.extractall(artifact["name"])
-        break
+    if not filtered_artifacts or len(filtered_artifacts) >1:
+        print(f"No artifacts found for this repository or multiple artifacts found - {designite_output_old}. Please check the artifact name.")
+        return False
+
+    artifact = filtered_artifacts[0]
+
+    resp = api_request(artifact["archive_download_url"], token)
+    if resp.status_code != 200:
+        print(f"Failed to download artifact '{artifact['name']}'.")
+        return False
+    try:
+        # Save the downloaded artifact to a local file - This saves in the current repository checkout directory (/github/workspace)
+        with open(f'{artifact["name"]}.zip', 'wb') as f:
+            f.write(resp.content)
+
+        os.makedirs(artifact["name"], exist_ok=True)
+        # Extract the downloaded artifact
+        with zipfile.ZipFile(f'{artifact["name"]}.zip', 'r') as zip_ref:
+            zip_ref.extractall(artifact["name"])
+    except Exception as e: #pylint: disable=broad-except
+        print(f"Failed to extract artifact '{artifact['name']}'.")
+        print(e)
+        return False
+
     return True
 
 def get_new_smells(designite_output_old, designite_output_new):
     '''Get the new smells from the current run'''
     process(designite_output_old, designite_output_new, "new_smells.json")
-    with open("new_smells.json", "r") as f:
+    with open("new_smells.json", "r") as f: #pylint: disable=unspecified-encoding
         print(f.read())
 
 
 def main(token, designite_output_old, designite_output_new, repo):
     '''Download an artifact from a given run ID.'''
+    
     artifact_resp = api_request(f"{GITHUB_API_URL}/repos/{repo}/actions/artifacts", token, params={"per_page": 100})
 
     if artifact_resp.status_code != 200:
         print(f"Failed to fetch artifacts for repository - {repo}.")
         return
-    
+
     artifact_resp = artifact_resp.json()
-    
+
     if artifact_resp.get("total_count", 0) == 0:
         print(f"No artifacts found for this repository - {repo}.")
         return
@@ -59,7 +70,7 @@ def main(token, designite_output_old, designite_output_new, repo):
     if not download_artifact(artifact_resp["artifacts"], designite_output_old, token):
         print(f"Failed to download artifact for repository - {repo}.")
         return
-    
+
     print(f"Artifact '{designite_output_old}' downloaded successfully.")
 
     get_new_smells(designite_output_old, designite_output_new)
