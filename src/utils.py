86255d534518d73091999eeeb5752319b49ed115
Indranil <indranil1102@gmail.com>
2023-12-14 01:51:41 -0400

    chore: print headers

--- src/utils.py
+++ src/utils.py
@@ -11,8 +11,10 @@ class Utils:
         headers = {"Authorization": f"Bearer {token}"}
         timeout = 10
         response = requests.request(method, url, headers=headers, timeout=timeout, data=data, params=params)
-        if int(response.headers.get("x-ratelimit-remaining",0)) < 10:
-            print("API rate limit low. Waiting for a minute")
-            time.sleep(60)
+        # if int(response.headers.get("x-ratelimit-remaining",0)) < 10:
+        #     print("API rate limit low. Waiting for a minute")
+        #     time.sleep(60)
+
+        print(response.headers)
         
         return response
